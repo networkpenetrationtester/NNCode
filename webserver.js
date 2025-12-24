b8e1d4085f2499dad1197340914151d68c67c1a8
networkpenetrationtester <x.nn77296.x@gmail.com>
2025-12-23 19:58:51 -0600

    git rekt lol

--- webserver.js
+++ webserver.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const fs = require("fs")
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
-app.get("/index.js", (req, res) => res.sendFile(`${__dirname}\\index.js`));
-app.get("/bad.woff", (req, res) => res.sendFile(`${__dirname}\\bad.woff`));
-app.get("/comic.woff", (req, res) => res.sendFile(`${__dirname}\\comic.woff`));
-app.get("/consolas.woff", (req, res) => res.sendFile(`${__dirname}\\consolas.woff`));
-app.get("/typewriter.woff", (req, res) => res.sendFile(`${__dirname}\\typewriter.woff`));
+app.get('/index.js', (req, res) => res.sendFile(path.join(__dirname, 'index.js')));
+app.get('/bad.woff', (req, res) => res.sendFile(path.join(__dirname, 'bad.woff')));
+app.get('/comic.woff', (req, res) => res.sendFile(path.join(__dirname, 'comic.woff')));
+app.get('/consolas.woff', (req, res) => res.sendFile(path.join(__dirname, 'consolas.woff')));
+app.get('/typewriter.woff', (req, res) => res.sendFile(path.join(__dirname, 'typewriter.woff')));
 
-app.get("*any", (req, res) => {
+app.get('*any', (req, res) => {
     res.sendFile(`${__dirname}\\index.html`);
 });
 
 app.listen(6767, () => {
-    console.log("Started webserver.");
+    console.log('Started webserver.');
 });
